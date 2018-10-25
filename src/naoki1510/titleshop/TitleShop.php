55fbe473bbd9725901de252060e072255d95456f
naoki1510 <15890587+naoki1510@users.noreply.github.com>
2018-10-25 23:45:43 +0900

    plugin beta complete

--- src/naoki1510/titleshop/TitleShop.php
+++ src/naoki1510/titleshop/TitleShop.php
@@ -1,102 +1,169 @@
-<?php 
+<?php
 
 namespace naoki1510\titleshop;
 
-use pocketmine\plugin\PluginBase;
-use pocketmine\utils\{ TextFormat, Config };
+/** @todo remove not to use. */
+use onebone\economyapi\EconomyAPI;
+use pocketmine\Player;
+use pocketmine\Server;
+use pocketmine\block\Block;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\entity\Effect;
+use pocketmine\entity\EffectInstance;
 use pocketmine\event\Listener;
-use pocketmine\event\player\{ PlayerInteractEvent, PlayerJoinEvent };
-use pocketmine\event\block\{ SignChangeEvent, BlockBreakEvent };
+use pocketmine\event\block\SignChangeEvent;
+use pocketmine\event\entity\EntityDamageByEntityEvent;
+use pocketmine\event\entity\EntityDamageEvent;
+use pocketmine\event\entity\EntityLevelChangeEvent;
+use pocketmine\event\entity\ProjectileLaunchEvent;
+use pocketmine\event\player\PlayerDeathEvent;
+use pocketmine\event\player\PlayerDropItemEvent;
+use pocketmine\event\player\PlayerInteractEvent;
+use pocketmine\event\player\PlayerItemUseEvent;
+use pocketmine\event\player\PlayerRespawnEvent;
+use pocketmine\event\server\DataPacketReceiveEvent;
+use pocketmine\item\Armor;
+use pocketmine\item\Bow;
+use pocketmine\item\Item;
+use pocketmine\item\Sword;
+use pocketmine\item\enchantment\Enchantment;
+use pocketmine\item\enchantment\EnchantmentInstance;
+use pocketmine\level\Explosion;
+use pocketmine\network\mcpe\protocol\ModalFormRequestPacket;
+use pocketmine\network\mcpe\protocol\ModalFormResponsePacket;
+use pocketmine\plugin\PluginBase;
+use pocketmine\tile\Sign;
+use pocketmine\utils\Config;
+use naoki1510\nametagapi\NameTagAPI;
 
-class TagShop extends PluginBase implements Listener
+
+class TitleShop extends PluginBase implements Listener
 {
-	public function onEnable()
-	{
-		$this->getServer()->getPluginManager()->registerEvents($this, $this);
-        $this->saveConfig();
-		$this->data = new Config($this->getDataFolder() . "TitleData.yml", Config::YAML);
-	}
+    /** @var Weapon[] */
+    //private $listeners;
 
-	public function onSignChange(SignChangeEvent $event)
-	{
-		$player = $event->getPlayer();
-		$line = $event->getLines();
-		if ($line[0] === "title" or $line[0] === TextFormat::GREEN . "[TitleShop]") {
-			if (!$player->isOp()) {
-				//$player->sendMessage(TextFormat::YELLOW . "あなたはタグショップを作成する権限がありません。");
-				return false;
-			}
-			$tag = $line[1];
-			$price = $line[2];
-			$event->setLine(0, TextFormat::GREEN . "[TitleShop]");
-			$event->setLine(1, TextFormat::WHITE .  $tag);
-			$event->setLine(2, TextFormat::YELLOW . "cost: " . $api->getMonitorUnit() . $price);
-		}
-	}
+    /** @var string[] */
+    private $cue = [];
 
-	public function onTouch(PlayerInteractEvent $event)
-	{
-		$player = $event->getPlayer();
-		$name = $player->getName();
-		$api = API::getInstance();
-        if ($event->getBlock()->getID() == 323 || $event->getBlock()->getID() == 63 || $event->getBlock()->getID() == 68) {
-            $sign = $player->getLevel()->getTile($event->getBlock())->getText();
-            $tag = ltrim($sign[1], TextFormat::AQUA . "タグ: " . TextFormat::RESET);
-            $price = ltrim($sign[2], TextFormat::YELLOW . "販売価格: " . $api->getMonitorUnit());
-            $description = $sign[3];
-            if ($sign[0] == TextFormat::GREEN . "[TAGSHOP]") {
-            	if ($api->Check($player) < $price) {
-            		$player->sendMessage(TextFormat::YELLOW . "所持金が不足しています。");
-            		return false;
-            	}
-            	if (!$this->buy[$name]) {
-            		$player->sendMessage(TextFormat::GREEN . "本当にタグ: " . TextFormat::RESET . $tag . TextFormat::GREEN . "を購入しますか？");
-            		$this->buy[$name] = true;
-            		return true;
-            	} else {
-	            	if ($api->takeMoney($player, $price)) {
-	            		$player->setNameTag(TextFormat::AQUA . "[" . TextFormat::RESET . $tag . TextFormat::AQUA . "] " . TextFormat::RESET . $name);
-	            		$player->setDisplayName(TextFormat::AQUA . "[" . TextFormat::RESET . $tag . TextFormat::AQUA . "] " . TextFormat::RESET . $name);
-	            		$this->data->set($name, $tag);
-	            		$this->data->save();
-	            		$player->sendMessage(TextFormat::GREEN . "タグを購入しました。");
-	            		$this->buy[$name] = false;
-	            		return true;
-	            	} else {
-	            		$player->sendMessage(TextFormat::RED . "タグ購入時にエラーが発生しました。");
-	            		$this->buy[$name] = false;
-	            		return false;
-	            	}
-	            }
-            }
+    public function onEnable()
+    {
+		// 起動時のメッセージ
+        $this->getLogger()->info("§eTitleShop was loaded.");
+        $this->saveDefaultConfig();
+		// イベントリスナー登録
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+    }
+    
+    /* 購入処理系 */
+
+    public function onPlayerTap(PlayerInteractEvent $e)
+    {
+        $player = $e->getPlayer();
+        // スニークしてる時はパス
+        if ($player->isSneaking()) return;
+
+        $block = $e->getBlock();
+        switch ($block->getId()) {
+            // 看板のID
+            case Block::WALL_SIGN:
+            case Block::SIGN_POST:
+                $sign = $block->getLevel()->getTile($block->asPosition());
+                // 看板の取得に失敗した時
+                if (!$sign instanceof Sign) return;
+                // 1行目がTitle, TitleShopじゃない時
+                if (preg_match('/^(§[0-9a-fklmnor])*\[Title(Shop)?\]$/iu', trim($sign->getLine(0))) != 1) return;
+                // コストをConfigから取得
+                $cost = $this->getConfig()->get('cost', 1000);
+                // Kit購入
+                $this->buy($player);
+                
+                break;
+
+            default:
+                return;
+                break;
         }
-	}
+        // ブロック配置の防止
+        $e->setCancelled();
+    }
 
-	public function onBreak(BlockBreakEvent $event)
-	{
-		$player = $event->getPlayer();
-		$name = $player->getName();
-        if ($event->getBlock()->getID() == 323 || $event->getBlock()->getID() == 63 || $event->getBlock()->getID() == 68) {
-            $sign = $player->getLevel()->getTile($event->getBlock())->getText();
-            if ($sign[0] == TextFormat::GREEN . "[TAGSHOP]") {
-            	if (!$player->isOp()) {
-            		$player->sendMessage(TextFormat::RED . "あなたはタグショップを破壊する権限がありません。");
-            		$event->setCancelled();
-            		return false;
-            	}
-            	return true;
+    public function onSignChange(SignChangeEvent $e)
+    {
+        if (preg_match('/^(§[0-9a-fklmnor])*\[?Title(Shop)?\]?$/iu', trim($e->getLine(0))) == 1) {
+            if(!$e->getPlayer()->isOp()){
+                $e->getPlayer()->sendMessage('You must be op to create shop sign.');
+                $e->setLines(['You must be op.', 'You must be op.', 'You must be op.', 'You must be op.']);
+                return;
             }
+            $this->reloadSign($e);
         }
-	}
+    }
 
-	public function onJoin(PlayerJoinEvent $event) {
-		$player = $event->getPlayer();
-		$name = $player->getName();
-		if ($this->data->exists($name)) {
-			$tag = $this->data->get($name);
-    		$player->setNameTag(TextFormat::AQUA . "[" . TextFormat::RESET . $tag . TextFormat::AQUA . "] " . TextFormat::RESET . $name);
-    		$player->setDisplayName(TextFormat::AQUA . "[" . TextFormat::RESET . $tag . TextFormat::AQUA . "] " . TextFormat::RESET . $name);
-		}
-		$this->buy[$name] = false;
-	}
+    /** @param SignChangeEvent|Sign $sign */
+    public function reloadSign($sign)
+    {
+        try {
+            if (preg_match('/^(§[0-9a-fklmnor])*\[?Title(Shop)?\]?$/iu', trim($sign->getLine(0))) == 1) {
+                preg_match('/^(§[0-9a-fklmnor])*(.*)$/u', trim($sign->getLine(1)), $m);
+                
+                $sign->setLine(0, '§a[TitleShop]');
+                $sign->setLine(1, '§l自由に称号を変更できます。');
+                $sign->setLine(2, '§c一度の変更につき$' . $this->getConfig()->get('cost', 1000) . 'かかります。');
+            }
+        } catch (\BadMethodCallException $e) {
+            // $signから文字を変更できなかった時
+            $this->getLogger()->warning($e->getMessage());
+        }
+
+    }
+
+    /** 購入 */
+    public function buy(Player $player) : bool
+    {
+        $cost = $this->getConfig()->get('cost');
+        // お金が足りるか
+        if ((EconomyAPI::getInstance()->myMoney($player) ?? 0) < $cost) {
+            $player->sendMessage('お金が足りません。');
+            return false;
+        }
+        // キューが空の時
+        if (empty($this->cue[$player->getName()])) {
+            $this->cue[$player->getName()] = true;
+            //購入確認フォーム
+            $pk = new ModalFormRequestPacket();
+            $pk->formId = INT32_MAX - 1510;
+            $form['type'] = "custom_form";
+            $form['title'] = '称号の変更';
+            $form['content'] = [["type" => "input", "text" => "称号をここに入力してください。"]];
+            $pk->formData = json_encode($form);
+            $player->dataPacket($pk);
+            return true;
+        }
+        return false;
+    }
+
+    /** 
+     * フォーム受信
+     */
+    public function onRecievePacket(DataPacketReceiveEvent $ev)
+    {
+        $pk = $ev->getPacket();
+        $player = $ev->getPlayer();
+
+        if (!$pk instanceof ModalFormResponsePacket) return;
+        if ($pk->formId !== INT32_MAX - 1510) return;
+
+        $data = json_decode($pk->formData, true);
+        if ($data === null){
+            $player->sendMessage("購入をキャンセルしました。");
+            $this->cue[$player->getName()] = null;
+            return;
+        } 
+        if(isset($data[0])){
+            NameTagAPI::getInstance()->setTag($this, $player, '[' . $data[0] . '§r]', NameTagAPI::POS_LEFT);
+            $player->sendMessage("Your tag was changed.");
+        }
+        $this->cue[$player->getName()] = null;
+    }
 }
